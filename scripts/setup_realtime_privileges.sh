38e5e2914770f1026dd43bb47217f636b57d9d2b
magonano <effenano@gmail.com>
2026-01-31 22:20:06 +0100

    Add copyright headers to all project files

--- scripts/setup_realtime_privileges.sh
+++ scripts/setup_realtime_privileges.sh
@@ -1,3 +1,18 @@
+# Copyright (C) 2024 Francesco Nano and AI
+# 
+# This file is part of the Open Live Mixing System (OLMS) project.
+# Created by Francesco Nano with AI assistance at https://openlivemixingsystem.org/
+#
+# Connect, collaborate, and stay updated with announcements at:
+# https://openlivemixingsystem.org/
+#
+# Licensed under the GNU General Public License v3.0 (GPL-3.0)
+# See LICENSE file for full license terms and conditions.
+#
+# This software is provided 'as-is', without any express or implied warranty.
+# In no event will the authors be held liable for any damages arising from
+# the use of this software.
+
 #!/bin/bash
 
 # OLMS Realtime Privileges Setup Script
